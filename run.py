2ca529009e664ab74f8602d6a633605c24aea885
PolyLvst <zeerofive05@gmail.com>
2025-02-27 14:09:22 +0700

    refactor run

--- run.py
+++ run.py
@@ -1,20 +1,35 @@
-import tokenize_util
+from tokenize_util import TokenizeUtil
+from prepare_dict import PrepareDictBuilder
+from db import DatabaseInserter
 import glob
 import os
 
-# class PipelineRun:
-#     def __init__(self):
+class PipelineRun:
+    def __init__(self, input_folder):
+        self.database_insert = DatabaseInserter()
+        self.input_folder = input_folder
+        self.__check_folder()
 
-# parsed_list = parse.ParseFromPDF("./sample.pdf").parse().output_as_list()
-# print(parsed_list)
-# with open("hasil-fitz.txt", "r") as file:
-#     for line in file:
+    def run(self):
+        pdf_files = glob.glob(f"{self.input_folder}/*.pdf")
+        for file in pdf_files:
+            print(f"->> Parsing file [{file}] ...")
 
-input_folder = "input"
-if not os.path.exists(input_folder):
-    os.mkdir(input_folder)
-pdf_files = glob.glob(f"{input_folder}/*.pdf")
-for file in pdf_files:
-    dict_parsed = tokenize_util.TokenizeUtil(file, "0000000").tokenize().output_as_dict()
-    # TODO insert to db
-    print(f"{dict_parsed.popitem()}")
+            dict_parsed = TokenizeUtil(file).tokenize().output_as_dict()
+            source = os.path.basename(file)
+            # TODO get from parsed pdf
+            account_number = "00000"
+            for key,value in dict_parsed.items():
+                value = PrepareDictBuilder(value).set_acno(account_number).set_source(source).build()
+                self.database_insert.append(value)
+
+            self.database_insert.insert()
+            print("->> Inserted to db ...")
+    
+    def __check_folder(self):
+        if not os.path.exists(self.input_folder):
+            os.mkdir(self.input_folder)
+
+if __name__ == "__main__":
+    runner = PipelineRun("./input")
+    runner.run()
